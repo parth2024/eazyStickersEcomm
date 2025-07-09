864642e06b70e97172702e6036c86003dc436ff2
parth2024 <156824891+parth2024@users.noreply.github.com>
2025-07-09 11:19:40 +0530

    Update EazyStoreSecurityConfig.java

--- eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -86,10 +86,10 @@ public class EazyStoreSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         // Allow requests from any origin
-        // config.setAllowedOriginPatterns(Collections.singletonList("*"));
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://eazystickersecomm-main-8e3e201.kuberns.cloud/"));
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        // config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://eazystickersecomm-main-8e3e201.kuberns.cloud/"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        config.setAllowCredentials(true); // Allow credentials (cookies, authorization tokens)
+        // config.setAllowCredentials(true); // Allow credentials (cookies, authorization tokens)
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept")); // Allow necessary headers
         config.setExposedHeaders(Arrays.asList("Authorization")); // Expose Authorization header for client access
         config.setMaxAge(3600L); // Caching pre-flight requests for 1 hour
